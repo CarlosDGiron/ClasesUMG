32a870e00b1ec3d78fcbeac12813770b41cb2e90
CarlosDGiron <carlosdavidgironleonardo@gmail.com>
2023-07-31 19:30:46 -0600

    Commit desde Netbeans

---
+++ src/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author cana0
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // System.out.println("Hola Mundo");
+        Form1 f=new Form1();
+        f.show();
+    }
+    
+}
